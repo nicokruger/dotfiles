cd0c24ceed653c3d53d87abacdafcd3cb1aa6f24
Nico Kruger <nico.kruger@gmail.com>
2013-06-05 00:51:05 +0200

    better error handling

--- i3/myi3status.sh
+++ i3/myi3status.sh
@@ -4,8 +4,8 @@
 
 i3status | while :
 do
-        SONG=$(cmus-remote -Q | sed "s;tag ;;g" | sed "s; ;=;" | python -c "import sys; params=dict(line.strip().split('=') for line in sys.stdin.readlines()); print('%(artist)s - %(album)s - %(tracknumber)s - %(title)s' % params)")
-        read line
-        echo "${SONG} | $line" || exit 1
+    SONG=$(cmus-remote -Q 2> /dev/null| sed "s;tag ;;g" | sed "s; ;=;" | python -c "import sys; params=dict(line.strip().split('=') for line in sys.stdin.readlines()); params and print('%(artist)s - %(album)s - %(tracknumber)s - %(title)s' % params)")
+    read line
+    echo "${SONG} | $line" || exit 1
 done
 
