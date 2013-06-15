e5f85ca57906b819ae4ffe30aa426aad8755be63
Nico Kruger <nico.kruger@gmail.com>
2013-06-15 09:37:44 +0200

    better error handling in song display

--- i3/myi3status.sh
+++ i3/myi3status.sh
@@ -4,7 +4,7 @@
 
 i3status | while :
 do
-    SONG=$(cmus-remote -Q 2> /dev/null| sed "s;tag ;;g" | sed "s; ;=;" | python -c "import sys, collections; params=collections.defaultdict(lambda: 'N/A'); params.update(dict(line.strip().split('=') for line in sys.stdin.readlines())); params and print('%(artist)s - %(album)s - %(tracknumber)s - %(title)s' % params)")
+    SONG=$(cmus-remote -Q 2> /dev/null| sed "s;tag ;;g" | sed "s; ;=;" | python -c "import sys, collections; params=collections.defaultdict(lambda: 'N/A'); params.update(dict(line.strip().split('=') for line in sys.stdin.readlines())); any(filter(lambda x: x != 'N/A',params.values())) and print('%(artist)s - %(album)s - %(tracknumber)s - %(title)s' % params)")
     read line
     echo "${SONG} | $line" || exit 1
 done
