6854c66fd2c09341ff2fff1c0a337c1ca5b09951
Nico Kruger <nico.kruger@gmail.com>
2014-08-14 13:26:30 +0200

    Small tweaks

--- .wallpaper.sh
+++ .wallpaper.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find /home/nicok/stuff/wallpapers -type f -print0 | shuf -n1 -z | xargs -0 feh --bg-max
+#find /home/nicok/play/RedditImageGrab/wallpaper -type f -print0 | shuf -n1 -z | xargs -0 feh --bg-max
+feh --bg-max /home/nicok/play/RedditImageGrab/wallpaper/primary1.jpg
+
