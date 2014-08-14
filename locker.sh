5a89162fa78f6bcc4295e4efcf9fbfec182ac2d1
Nico Kruger <nico.kruger@gmail.com>
2014-08-14 21:57:51 +0200

    Locker

--- locker.sh
+++ locker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 WALL=$(find /home/nicok/play/RedditImageGrab/wallpaper -type f -print0 | shuf -n1 -z | xargs -0 echo)
 echo "Wall is: ${WALL}"
-TMP="$(mktemp).png"
-convert $WALL $TMP
+#TMP="$(mktemp).png"
+#convert $WALL $TMP
+
+TMP=$(/home/nicok/play/walle/a.py $WALL)
+
 i3lock -t -u -i "$TMP"
 
 
